e533db356a63f2d541d39e14d4c2114a4afc0541
Kavya185 <146148196+Kavya185@users.noreply.github.com>
2025-04-29 10:01:55 +0530

    Delete election_prediction directory

--- election_prediction/news_fetchers.py
+++
@@ -1,8 +0,0 @@
-from gnews import GNews
-
-def fetch_news(party_name, state=None):
-    google_news = GNews(language='en', max_results=20)
-    query = f"{party_name} election"
-    if state:
-        query = f"{party_name} {state} election"
-    return google_news.get_news(query)
